67764afce830ab529ae8b335f8dc13d6b2d56f7b
PanVovan <vova.antipenkov@gmail.com>
2021-05-05 19:19:32 +0300

    updated mqtt server v0.0.1a && created index.html

---
+++ src/client/maprender.js
@@ -0,0 +1,32 @@
+
+let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+}).addTo(mymap);
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+
+let circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
---
+++ src/trashbox.ts
@@ -0,0 +1,18 @@
+class Trashbox
+{
+    id;
+    fullness;
+    longitude;
+    latitude;
+
+    constructor (id)
+    {
+        this.id = id;
+        this.fullness = 0;
+        this.longitude = 0;
+        this.latitude = 0;
+    }
+};
+
+
+export {Trashbox};
