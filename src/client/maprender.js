ede6b4d458fe6ad069f1b1b17c00d39e945a32c4
PanVovan <vova.antipenkov@gmail.com>
2021-05-16 18:38:00 +0300

    for herokuapp

--- src/client/maprender.js
+++ src/client/maprender.js
@@ -79,8 +79,6 @@ const containerIcon = L.icon({
         iconUrl: `container.svg`,
     
         iconSize:     [40, 40], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
 // let circle = L.circle([51.508, -0.11], {
